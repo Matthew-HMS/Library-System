ddd03ef690c734cbb9be8afa808f6db13ff0eefa
imsho <92440104+Matthew-HMS@users.noreply.github.com>
2022-06-02 21:39:39 +0800

    Add files via upload

--- Library.java
+++ Library.java
@@ -7,7 +7,7 @@ public class Library {
         ArrayList<Users> users = new ArrayList<Users>();//array to store users
         Scanner s = new Scanner(System.in);
         int input = 0;
-        Users user = new Users();
+        Users user = new Student();
         Register r = new Register();
         int check = -1;//check = -1 means user is not login, check != 0 means user is login
         do{
@@ -41,24 +41,25 @@ public class Library {
             
             if(users.get(check).getIdentity().equals("Admin")){
                 System.out.println("您現在已登入為管理員");
-                System.out.println("輸入\n1.新增書籍\n2.修改書籍\n3.刪除書籍\n4.搜尋書籍\n5.查看個人資訊(所有借還記錄)\n6.登出\n7.離開");
+                System.out.println("輸入\n1.新增書籍\n2.修改書籍\n3.刪除書籍\n4.查詢書籍(借還記錄)\n5.查詢會員(借還記錄)\n6.登出\n7.離開");
+                Admin admin = new Admin();
                 input = s.nextInt();
                 do{
                     switch(input){
                         case 1:
-                            user.addBook();
+                            admin.addBook();
                             break;
                         case 2:
-                            user.EditBook();
+                            admin.EditBook();
                             break;
                         case 3:
-                            user.DeleteBook();
+                            admin.DeleteBook();
                             break;
                         case 4:
-                            user.searchBook();
+                            admin.searchBook();
                             break;
                         case 5:
-                            user.viewInfo();
+                            admin.viewInfo();
                             break;
                         case 6:
                             check = -1;
@@ -75,27 +76,37 @@ public class Library {
             }
             else{
                 
-                System.out.println("您現在已登入為會員");
-                System.out.println("輸入\n1.借書\n2.還書\n3.搜尋書籍\n4.查看個人資訊(借還記錄)\n5.登出\n6.離開");
+                
+                Member member = new Student();
+                if(users.get(check).getIdentity().equals("Student")){
+                    member = new Student();
+                    System.out.println("登入身分 : 學生");
+                }
+                else if(users.get(check).getIdentity().equals("Teacher")){
+                    member = new Teacher();
+                    System.out.println("登入身分 : 教師");
+                }
+                else if(users.get(check).getIdentity().equals("Staff")){
+                    member = new Staff();
+                    System.out.println("登入身分 : 職員");
+                }
+                System.out.println("輸入\n1.借書\n2.還書\n3.查詢書籍\n4.查看個人資訊(借還記錄)\n5.登出\n6.離開");
                 input = s.nextInt();
                 
-                if(users.get(check).getIdentity().equals("Student")){user = new Student();}
-                else if(users.get(check).getIdentity().equals("Teacher")){user = new Teacher();}
-                else if(users.get(check).getIdentity().equals("Staff")){user = new Staff();}
                 
                 do{
                     switch(input){
                         case 1:
-                            user.borrowBook();
+                            member.borrowBook();
                             break;
                         case 2:
-                            user.returnBook();
+                            member.returnBook();
                             break;
                         case 3:
-                            user.searchBook();
+                            member.searchBook();
                             break;
                         case 4:
-                            user.viewInfo();
+                            member.viewInfo();
                             break;
                         case 5:
                             check = -1;
