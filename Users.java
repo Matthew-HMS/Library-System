0eb2af23a2e7fd18eeae53c94dfcdcf0640366c3
ashleyy-huang <106752123+ashleyy-huang@users.noreply.github.com>
2022-06-07 20:07:34 +0800

    Update Users.java

--- Users.java
+++ Users.java
@@ -66,38 +66,27 @@ public abstract class Users {
 			searchWay = scan.nextLine();// 改成String 比較不會出錯
 			if (searchWay.equals("1")) {
 				System.out.println("請輸入查詢書籍之名稱:");
-				String bookName = scan.nextLine();
+				String bookName = scan.nextLine().toLowerCase();
 				//int remainNum = 0;// 計算還剩下幾本
 				int count = 0;
 				// 圖書館有這本書
 				for (int i = 0; i < booklist.size(); i++) {
 					if (booklist.get(i).getName().contains(bookName)) {
-						//remainNum++;//
 						System.out.printf("書名:%s\t作者:%s\t出版社:%s\tID:%s\t圖書分類:%s\t存放區域:%s\n",
 								booklist.get(i).getName(), booklist.get(i).getAuthor(),
 								booklist.get(i).getPub(), booklist.get(i).getId(), booklist.get(i).getType(),
 								booklist.get(i).getAddress());// 列印出所有相同名稱之書籍
 					}
-                    //if (remainNum > 0) {
-                    //	System.out.printf("總共還有%d可以借", remainNum);
-                    //} else {
-                    //	System.out.printf("十分抱歉，本館的%s都被借光了", bookName);
-                    //}
-
-					
 					// 圖書館沒有這本書
-					else {
-						count++;
-					}
-			
-					
+					else {count++;}
 				} // for_i
-                if (count == booklist.size()) {
-                    System.out.println("十分抱歉，本館沒有您所查詢之書籍");
-                }
+				
+               		 	if (count == booklist.size()) {
+                    		System.out.println("十分抱歉，本館沒有您所查詢之書籍");
+                		}
 				else{
-                    System.out.println("以上是你的搜尋結果");
-                }
+                   		 System.out.println("以上是你的搜尋結果");
+               			}
 			} // end if_1
 
 			else if (searchWay.equals("2")) {
