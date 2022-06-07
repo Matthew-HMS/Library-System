165a981dae45d4c53655c123849adc4da5fed5b4
yeh-alex <85924765+yeh-alex@users.noreply.github.com>
2022-06-07 20:09:44 +0800

    Add files via upload

--- Users.java
+++ Users.java
@@ -66,27 +66,38 @@ public abstract class Users {
 			searchWay = scan.nextLine();// 改成String 比較不會出錯
 			if (searchWay.equals("1")) {
 				System.out.println("請輸入查詢書籍之名稱:");
-				String bookName = scan.nextLine().toLowerCase();
+				String bookName = scan.nextLine();
 				//int remainNum = 0;// 計算還剩下幾本
 				int count = 0;
 				// 圖書館有這本書
 				for (int i = 0; i < booklist.size(); i++) {
 					if (booklist.get(i).getName().contains(bookName)) {
+						//remainNum++;//
 						System.out.printf("書名:%s\t作者:%s\t出版社:%s\tID:%s\t圖書分類:%s\t存放區域:%s\n",
 								booklist.get(i).getName(), booklist.get(i).getAuthor(),
 								booklist.get(i).getPub(), booklist.get(i).getId(), booklist.get(i).getType(),
 								booklist.get(i).getAddress());// 列印出所有相同名稱之書籍
 					}
+                    //if (remainNum > 0) {
+                    //	System.out.printf("總共還有%d可以借", remainNum);
+                    //} else {
+                    //	System.out.printf("十分抱歉，本館的%s都被借光了", bookName);
+                    //}
+
+					
 					// 圖書館沒有這本書
-					else {count++;}
+					else {
+						count++;
+					}
+			
+					
 				} // for_i
-				
-               		 	if (count == booklist.size()) {
-                    		System.out.println("十分抱歉，本館沒有您所查詢之書籍");
-                		}
+                if (count == booklist.size()) {
+                    System.out.println("十分抱歉，本館沒有您所查詢之書籍");
+                }
 				else{
-                   		 System.out.println("以上是你的搜尋結果");
-               			}
+                    System.out.println("以上是你的搜尋結果");
+                }
 			} // end if_1
 
 			else if (searchWay.equals("2")) {
@@ -122,20 +133,21 @@ public abstract class Users {
 
 	public void editMember(ArrayList<Users> users) {
         Scanner s = new Scanner(System.in);
-    	System.out.print("請輸入欲更改Member姓名 :");
-        String name = s.nextLine();
+    	System.out.print("請輸入欲更改Member帳號 :");
+        String account = s.nextLine();
     	int input = 0;
     	int count = 0;
     	for (int i = 0; i < users.size(); i++) {
-    		if(users.get(i).getName().equals(name)) {
+    		if(users.get(i).getAccount().equals(account)) {
     			count = 1;
     			while(input != 6) {
-    				System.out.print("要更改什麼?\n1.account\n2.password\n3.name\n4.email\n5.phone\n6.離開\n請輸入: ");
+    				System.out.print("要更改什麼?\n1.password\n2.name\n3.email\n4.phone\n5.離開\n請輸入: ");
     				String inputs = s.nextLine();
     				input = Integer.parseInt(inputs);
     				switch(input){
     				
-    				case 1:
+    				/* 
+						case 1:
 					
     					System.out.print("請輸入帳號 : ");
     					String account = s.nextLine();
@@ -146,35 +158,36 @@ public abstract class Users {
     						System.out.println("修改帳號完成!");
     					}
     					else {System.out.println("修改帳號失敗! 此帳號已存在");}
-    					break;							
+    					break;
+						*/							
     				
-					case 2:
+					case 1:
     					System.out.println("請輸入密碼 : ");
     					users.get(i).setPassword(s.nextLine());
     					System.out.println("修改密碼完成!");
     					break;
     				
-					case 3:
+					case 2:
     					System.out.println("請輸入名稱 : ");
     					users.get(i).setName(s.nextLine());
     					System.out.println("修改名稱完成!");
     					break;
     				    				    			
-    				case 4:
+    				case 3:
     					System.out.println("請輸入email : ");
     					users.get(i).setEmail(s.nextLine());
     					System.out.println("修改email完成!");
     		
     					break;
         
-    				case 5:
+    				case 4:
     					System.out.println("請輸電話 : ");
     					users.get(i).setPhone(s.nextLine());
     					System.out.println("修改電話完成!");
     					break;
     					
     				default :
-    					input = 6;
+    					input = 5;
     					System.out.println("離開修改系統!");
     					break;
     				}
