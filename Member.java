bf06b2ef558c646d2fa2b80031eeff74271f4f43
imsho <92440104+Matthew-HMS@users.noreply.github.com>
2022-06-08 14:48:02 +0800

    Add files via upload

--- Member.java
+++ Member.java
@@ -8,10 +8,6 @@ public class Member extends Users {
     public Member(String account, String password, String name, String email, String phone, String identity){
         super(account, password, name, email, phone, identity);
     }
-    
-    public String toString(){
-        return getAccount() + " " + getName() + " " + getPhone();
-    }
     public void viewInfo(ArrayList<Users> users, int check){
 		super.viewInfo(users, check);
 	}
