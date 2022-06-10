3aff8addf74a99728f162ac0df27f3a6c989a729
imsho <92440104+Matthew-HMS@users.noreply.github.com>
2022-06-11 02:01:34 +0800

    Add files via upload

--- Register.java
+++ Register.java
@@ -1,21 +1,21 @@
 
 import java.util.*;
+import javax.swing.*;
 
 public class Register {
     
     Scanner s = new Scanner(System.in);
     Random r = new Random();
-    //SendMail mail = new SendMail();
+    SendMail mail = new SendMail();
 
     public void register(ArrayList<Users> users){
         boolean repeat = true;
         boolean haveacc = false;
         do{
-            System.out.print("Your account number(National ID) : ");
-            String account = s.nextLine();
+            String account = JOptionPane.showInputDialog(null, "Your account number(National ID) : ", "Register", JOptionPane.QUESTION_MESSAGE);
             for (int i = 0; i < users.size(); i++){
                 if (account.equals(users.get(i).getAccount())){
-                    System.out.println("Register failed! This account has been used.");
+                    JOptionPane.showMessageDialog(null, "Register failed! This account has been used.", "Register", JOptionPane.ERROR_MESSAGE);
                     haveacc = true;
                     break;
                     
@@ -24,34 +24,37 @@ public class Register {
             }
             if(haveacc){haveacc = false;continue;}
 
-            System.out.print("Your password : ");
-            String password = s.nextLine();
-            System.out.print("confirm password : ");
-            String confirmpassword = s.nextLine();
+            String password = JOptionPane.showInputDialog(null, "Your password : ", "Register", JOptionPane.QUESTION_MESSAGE);
+            String confirmpassword = JOptionPane.showInputDialog(null, "Confirm password : ", "Register", JOptionPane.QUESTION_MESSAGE);
             if(!password.equals(confirmpassword)){
-                System.out.println("Register failed! Your password didn't match.");
+                JOptionPane.showMessageDialog(null, "Register failed! Your password didn't match.", "Register", JOptionPane.ERROR_MESSAGE);
                 continue;
             }
 
-            System.out.print("Your name : ");
-            String name = s.nextLine();
-            System.out.print("Your email : ");
-            String email = s.nextLine();
-            System.out.print("Your phone number: ");
-            String phone = s.nextLine();
-            System.out.print("Your identity : ");
-            String identity = s.nextLine();
+            String name = JOptionPane.showInputDialog(null, "Your name : ", "Register", JOptionPane.QUESTION_MESSAGE);
+            String email = JOptionPane.showInputDialog(null, "Your email : ", "Register", JOptionPane.QUESTION_MESSAGE);
+            String phone = JOptionPane.showInputDialog(null, "Your phone number : ", "Register", JOptionPane.QUESTION_MESSAGE);
+            String [] option = {"Admin", "Student", "Teacher", "Staff"};
+            int input = JOptionPane.showOptionDialog(null, "Your identity : ", "Register", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, null);
+
+            String identity = "";
+            if(input == 0){identity = "Admin";}
+            else if(input == 1){identity = "Student";}
+            else if(input == 2){identity = "Teacher";}
+            else if(input == 3){identity = "Staff";}
+            else{System.exit(0);}
+            
             if (account.equals("") || password.equals("") || name.equals("") || email.equals("") || phone.equals("") || identity.equals("")){
-                System.out.println("Register failed! You didn't input all information required.");
+                JOptionPane.showMessageDialog(null, "Register failed! You didn't input all information required.", "Register", JOptionPane.ERROR_MESSAGE);
                 repeat = false;
             }
             else if (!identity.equals("Admin") && !identity.equals("Student") && !identity.equals("Teacher") && !identity.equals("Staff")){
-                System.out.println("Register failed! Your identity is incorrect.");
+                JOptionPane.showMessageDialog(null, "Register failed! Your identity is incorrect.", "Register", JOptionPane.ERROR_MESSAGE);
                 repeat = false;
             }
             else {
 
-                /*
+                /* 
                 boolean confirm = false;
                 int [] code = new int[6];
                 for(int i = 0; i < 6; i++){
@@ -63,30 +66,27 @@ public class Register {
                 }
                 String text = "<h2>This is your verification code: </h2>" + "<h1>" + codeString + "</h1>";
                 mail.send(email,"Welcome to Central Library" ,text);
-                System.out.println("Your verification code has been sent to your email.");
-                System.out.print("Please input your verification code : ");
-                String input = s.nextLine();
-                if (input.equals(codeString)){
+                JOptionPane.showMessageDialog(null, "Your verification code has been sent to your email.", "Register", JOptionPane.INFORMATION_MESSAGE);
+                String pwInput = JOptionPane.showInputDialog(null, "Please input your verification code : ", "Register", JOptionPane.QUESTION_MESSAGE);
+                if (pwInput.equals(codeString)){
                     confirm = true;
                 }
 
                 if (confirm){
                     Users user = new Student(account, password, name, email, phone, identity);
-                    System.out.println("You have successfully registered! You will receive an email notification.");
-                    text = "<h1>Welomce,  " + name + " </h1><h2> This email notice you that you have succesfully registered in Central Library. </h2><h2>Your Registration information :</h2>Identity : "+ identity +"<br>Account number : " + account + "<br>Email : "+ email + "<br>Phone number : "+ phone  + "<br><br><br>Click <a href=\"https://www.lib.ncu.edu.tw/\">here</a> for more information about Central Library.<br> Click <a href=\"mailto:matthew.in.ncu@g.ncu.edu.tw\">here</a> if you want to contact us." ;
-                    //mail.send(email, "Your account has been activated", text);
+                    JOptionPane.showMessageDialog(null, "You have successfully registered! You will receive an email notification.", "Register", JOptionPane.INFORMATION_MESSAGE);
+                    text = "<h1>Welomce,  " + name + " </h1><h2> This email notice you that you have successfully registered in Central Library. </h2><h2>Your Registration information :</h2>Identity : "+ identity +"<br>Account number : " + account + "<br>Email : "+ email + "<br>Phone number : "+ phone  + "<br><br><br>Click <a href=\"https://www.lib.ncu.edu.tw/\">here</a> for more information about Central Library.<br> Click <a href=\"mailto:matthew.in.ncu@g.ncu.edu.tw\">here</a> if you want to contact us." ;
+                    mail.send(email, "Your account has been activated", text);
                     users.add(user);
                     repeat = false;
                 }
                 else{
-                    System.out.println("Register failed! Your verification code is incorrect.");
+                    JOptionPane.showMessageDialog(null, "Register failed! Your verification code is incorrect.", "Register", JOptionPane.ERROR_MESSAGE);
                     repeat = false;
                 }
                 */
                 Users user = new Student(account, password, name, email, phone, identity);
                 System.out.println("You have successfully registered! You will receive an email notification.");
-                //text = "<h1>Welomce,  " + name + " </h1><br><h2> This email notice you that you have succesfully registered in Central Library. </h2><br><h2>Your Registration information :</h2><br>Identity : "+ identity +"<br>Account number : " + account + "<br>Email : "+ email + "<br>Phone number : "+ phone  + "<br><br><br>Click <a href=\"https://www.lib.ncu.edu.tw/\">here</a> for more information about Central Library.<br> Click <a href=\"mailto:matthew.in.ncu@g.ncu.edu.tw\">here</a> if you want to contact us." ;
-                //mail.send(email, "Your account has been activated", text);
                 users.add(user);
                 repeat = false;
 
@@ -97,24 +97,22 @@ public class Register {
     
     public int login(ArrayList<Users> users){
         
-        System.out.println("Please input your account number : ");
-        String account = s.nextLine();
+        String account = JOptionPane.showInputDialog(null, "Please input your account number : ", "Login", JOptionPane.QUESTION_MESSAGE);
         for(int i = 0; i < users.size(); i++){
             if(users.get(i).getAccount().equals(account)){
-                System.out.println("Please input your password : ");
-                String password = s.nextLine();
+                String password = JOptionPane.showInputDialog(null, "Please input your password : ", "Login", JOptionPane.QUESTION_MESSAGE);
                 if(users.get(i).getPassword().equals(password)){
-                    System.out.println("Login successfully!");
+                    JOptionPane.showMessageDialog(null, "Login successfully!", "Login", JOptionPane.INFORMATION_MESSAGE);
                     return i;
                 }
                 else{
-                    System.out.println("Wrong password!");
+                    JOptionPane.showMessageDialog(null, "Wrong password!", "Login", JOptionPane.ERROR_MESSAGE);
                     return -1;
                 }
             }
             
         }
-        System.out.println("No such account!");
+        JOptionPane.showMessageDialog(null, "No such account!", "Login", JOptionPane.ERROR_MESSAGE);
         return -1;
     }
     
